e86e860aa07bf9e3ddecb287e194263fed983859
n9Udq1IRE <nixiwafic.qamugysol@unimedia.fr>
2014-02-07 16:14:34 +0100

    1

--- Identity.cs
+++ Identity.cs
@@ -9,6 +9,7 @@ namespace spe.main
 {
     class Identity
     {
+        // Test
         private Random Random = new Random();
         private char[] __entiers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         private char[] __majuscules = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
